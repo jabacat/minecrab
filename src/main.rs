1a60cd663845bb4bb2016795c0f60e32848a4886
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2026-05-22 15:47:24 -0700

    fix: properly hide cursor, disable vsync

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ fn main() {
     let (mut rl, thread) = raylib::init()
         .size(WINDOW_WIDTH, WINDOW_HEIGHT)
         .title("Hello, world!")
-        .vsync()
+        // FIXME: flickering
+        //.vsync()
         .build();
 
     rl.disable_cursor();
-    rl.hide_cursor();
 
     let mut camera = Camera3D::perspective(
         Vector3::new(3.0, 3.0, 3.0),
@@ -24,7 +24,7 @@ fn main() {
         rl.update_camera(&mut camera, CameraMode::CAMERA_FIRST_PERSON);
         
         rl.draw(&thread, |mut d| {
-            d.clear_background(Color::BLACK);
+            d.clear_background(Color::LIGHTBLUE);
 
             d.draw_mode3D(camera, |mut d2, _camera| {
                 d2.draw_cube(Vector3::new(0.0, 0.0, 0.0), 1.0, 1.0, 1.0, Color::WHITE);
