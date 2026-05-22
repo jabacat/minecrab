9d7807bc0230e6d35ee2f9cb319f183a314f05cd
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2026-05-22 15:22:31 -0700

    wip: render unit cube

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use raylib::prelude::*;
+
+const WINDOW_WIDTH: i32 = 1280;
+const WINDOW_HEIGHT: i32 = 720;
+
 fn main() {
-    println!("Hello, world!");
+    let (mut rl, thread) = raylib::init()
+        .size(WINDOW_WIDTH, WINDOW_HEIGHT)
+        .title("Hello, world!")
+        .vsync()
+        .build();
+
+    rl.disable_cursor();
+    rl.hide_cursor();
+
+    let mut camera = Camera3D::perspective(
+        Vector3::new(3.0, 3.0, 3.0),
+        Vector3::new(0.0, 0.0, 0.0),
+        Vector3::new(0.0, 1.0, 0.0),
+        45.0,
+    );
+
+    while !rl.window_should_close() {
+        rl.update_camera(&mut camera, CameraMode::CAMERA_FIRST_PERSON);
+        
+        rl.draw(&thread, |mut d| {
+            d.clear_background(Color::BLACK);
+
+            d.draw_mode3D(camera, |mut d2, _camera| {
+                d2.draw_cube(Vector3::new(0.0, 0.0, 0.0), 1.0, 1.0, 1.0, Color::WHITE);
+            });
+        });
+    }
 }
