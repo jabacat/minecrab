b0edf222f22366a31b4275a1f5fc2e4b0ac659f5
Brandon Qi <71684640+Sophon96@users.noreply.github.com>
2026-05-24 02:21:58 -0700

    Chunk generation and rendering (#13)
    * feat: add texture atlas (@JakeRoggenbuck)
    
    * wip: chunk generation and clumped rendering
    
    * refactor: use VecMesh (also fixes UAF on exit)
    
    * fix: replace texture atlas with original (sorry Jake, I'm dumb)
    
    * feat: texture sampling
    
    * refactor: significantly simplify chunk mesh gen by removing floodfill
    
    * refactor: remove commented out code
    
    * refactor: separate world gen into distinct module
    
    * refactor: missing newline
    
    * feat: use x y z offsets in chunk generation
    
    * feat: generate multiple chunks
    
    * refactor: abstract out block generation

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
 use raylib::prelude::*;
 
-pub mod mesh_tools;
+use crate::world::generation::generate_chunk;
 
-use mesh_tools::VecMesh;
+mod mesh_tools;
+mod world;
 
 const WINDOW_WIDTH: i32 = 1280;
 const WINDOW_HEIGHT: i32 = 720;
-
-/* example of generating a triangle from raylib examples page */
-fn gen_custom_mesh() -> Mesh {
-    let mut vmesh = VecMesh::new();
-
-    vmesh.vertices = vec![ 0.0, 0.0, 0.0, /**/ 1.0, 0.0, 2.0, /**/ 2.0, 0.0, 0.0];
-    vmesh.normals = vec![ 0.0, 1.0, 0.0, /**/ 0.0, 1.0, 0.0, /**/ 0.0, 1.0, 0.0];
-    vmesh.texcoords = vec![0.0, 0.0, /**/ 0.5, 1.0, /**/ 1.0, 0.0];
-
-    let mut mesh = vmesh.to_mesh();
-
-    unsafe { mesh.upload(false); }
-    return mesh;
-}
 
 fn main() {
     let (mut rl, thread) = raylib::init()
@@ -39,8 +26,31 @@ fn main() {
     let mut first_click = false;
     let mut debug_display = false; // toggle
 
-    let mesh = gen_custom_mesh();
-    let material = rl.load_material_default(&thread);
+    let texture = unsafe {
+        let mut t = rl.load_texture(&thread, "assets/full-textures.png").unwrap();
+        t.gen_texture_mipmaps();
+        t.unwrap()
+    };
+
+    let mut models: Vec<Model> = Vec::new();
+    for cx in -4..4 {
+        for cy in -4..4 {
+            for cz in -4..4 {
+                models.push(generate_chunk(&mut rl, &thread, cx, cy, cz));
+            }
+        }
+    }
+    // FIXME: I will be back one day, borrow checker...
+    // let rl_ref = & rl;
+    // let thread_ref = &thread;
+    // let mut models: Vec<Model> = (-8..8).flat_map(|cx| (-8..8).flat_map(move |cy| (-8..8).map(move |cz| generate_chunk(rl_ref, thread_ref, cx, cy, cz)))).collect();
+    
+    models.iter_mut().for_each(|model| {
+        let materials = model.materials_mut();
+        let material = &mut materials[0];
+        let maps = material.maps_mut();
+        maps[MaterialMapIndex::MATERIAL_MAP_ALBEDO as usize].texture = texture;
+    });
 
     while !rl.window_should_close() {
         // require a click on the window before updating camera so the camera
@@ -60,25 +70,27 @@ fn main() {
         rl.draw(&thread, |mut d| {
             d.clear_background(Color::LIGHTBLUE);
 
+            d.draw_mode3D(camera, |mut d2, _camera| {
+                for model in &models {
+                    d2.draw_model(model, Vector3::zero(), 1., Color::WHITE);
+                }
+            });
+
             if !first_click {
                 d.draw_text("WIP: Click to start updating camera", 20, 20, 16, Color::DARKGREEN);
             }
             if debug_display {
                 let mut debug_info = String::new();
                 debug_info += &format!(
-                    "Camera position: {:.4} {:.4} {:.4}\n", 
+                    "Camera position: {:.4} {:.4} {:.4}\n",
                     camera.position.x, camera.position.y, camera.position.z
                 );
                 debug_info += &format!(
-                    "FPS: {}\n", 
+                    "FPS: {}\n",
                     d.get_fps()
                 );
                 d.draw_text(&debug_info, 20, 20, 16, Color::DARKGREEN);
             }
-
-            d.draw_mode3D(camera, |mut d2, _camera| {
-                d2.draw_mesh(&mesh, material.clone(), Matrix::identity());
-            });
         });
     }
 }
