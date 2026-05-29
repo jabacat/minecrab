86b05762b08ba865f82b6611aea0338706baacf4
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2026-05-29 16:01:32 -0700

    fix: must build against SDL3

--- scripts/macos-setup.sh
+++ scripts/macos-setup.sh
@@ -2,8 +2,21 @@
 # Run this script once per command line session, and thereafter run 'cargo build'
 # and 'cargo run' plain to compile and execute the project.
 
-# This is needed for SDL2 linkage errors.
+# This is needed for SDL3 linkage errors.
 
-# This assumes you are using homebrew for your SDL2 installation.
+# This assumes you are using homebrew for your SDL3 installation.
 
-export RUSTFLAGS="-L $(brew --prefix sdl2)/lib -l sdl2"
+if ! brew ls --versions sdl3 &> /dev/null;
+then
+	echo "SDL3 not installed via brew: doing nothing"
+	return 1
+fi
+
+if brew ls --versions pkg-config &> /dev/null;
+then
+	# prefer to use pkg-config to detect correct compile flags
+	export RUSTFLAGS="${RUSTFLAGS}  $(pkg-config --libs sdl3)"
+else
+	# fallback to old method
+	export RUSTFLAGS="-L $(brew --prefix sdl3)/lib -l sdl3"
+fi
